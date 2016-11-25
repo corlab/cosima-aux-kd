de7d2d16f7e86cda75c75baa6cf325fe1feeef0c
xwavex <xwavedw@gmail.com>
2016-11-25 16:47:43 +0100

    real robot

---
+++ src/inverse-dynamics-tmp.cpp
@@ -0,0 +1,279 @@
+#include <rtt/Operation.hpp>
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include "inverse-dynamics-tmp.hpp"
+
+using namespace cosima;
+using namespace RTT;
+using namespace RTT::os;
+using namespace Eigen;
+
+InverseDynamicsTMP::InverseDynamicsTMP(const std::string &name) :
+		TaskContext(name), _models_loaded(false), jointFB_Flow(RTT::NoData), inertia_Port(
+				"inertia"), h_Port("hVector"), jointFB_Port("jointFB"), gravity_vector(
+						0,0,-9.81), robotInertia_Port("robotInertia"), robotInertia_Flow(
+				RTT::NoData), useRobotInertia(false), receiveTranslationOnly(true) {
+
+	this->addOperation("loadURDFAndSRDF", &InverseDynamicsTMP::loadURDFAndSRDF,
+			this, RTT::ClientThread);
+
+	this->addOperation("getKinematicChainNames",
+			&InverseDynamicsTMP::getKinematicChainNames, this, RTT::ClientThread);
+
+    this->addOperation("setDOFsize", &InverseDynamicsTMP::setDOFsize, this, RTT::ClientThread).doc("set DOF size");
+    this->addOperation("setTranslationOnly", &InverseDynamicsTMP::setTranslationOnly, this, RTT::ClientThread).doc("set translation only, or use also orientation");
+    this->addOperation("computeGravity", &InverseDynamicsTMP::computeGravity, this, RTT::ClientThread).doc("compute gravity compensating torques");
+    this->addOperation("setBaseAndTip", &InverseDynamicsTMP::setBaseAndTip,this,RTT::ClientThread).doc("Set base and tip of the kinematic chain");
+	this->addOperation("setGravityVector", &InverseDynamicsTMP::setGravityVector,this,RTT::ClientThread).doc("Set gravity vector direction and magnitude");
+    this->ports()->addPort(inertia_Port).doc("Sending inertia.");
+
+	this->ports()->addPort(h_Port).doc(
+			"Sending calculated h vector containing Coriolis plus Gravity.");
+
+	this->ports()->addPort(jointFB_Port).doc("Receiving joint feedback.");
+
+	this->addOperation("useRobotInertia", &InverseDynamicsTMP::useRobotInertia_func,
+			this, RTT::OwnThread).doc("Use the inerati provided by the robot.").arg(
+			"useRobotInertia", "use the inertia from the robot or not.");
+
+	setTranslationOnly(true);
+}
+
+void InverseDynamicsTMP::setTranslationOnly(const bool translationOnly) {
+    receiveTranslationOnly = translationOnly;
+    if(receiveTranslationOnly) {
+        TaskSpaceDimension = 3;
+    }
+    else{
+        TaskSpaceDimension = 6;
+    }
+}
+
+void InverseDynamicsTMP::setDOFsize(unsigned int DOFsize){
+    assert(DOFsize > 0);
+    this->DOFsize = DOFsize;
+}
+
+void InverseDynamicsTMP::useRobotInertia_func(bool useRobotInertia) {
+	this->useRobotInertia = useRobotInertia;
+}
+
+void InverseDynamicsTMP::updateHook() {
+
+    jointFB_Flow = jointFB_Port.read(jointFB);
+    if (jointFB_Flow == RTT::NewData) {
+        calculateDynamics(jointFB);
+    }
+
+    // if (useRobotInertia) {
+    //     robotInertia_Flow = robotInertia_Port.read(inertia);
+    //     if (robotInertia_Flow != RTT::NoData) {
+    //         inertia_Port.write(inertia);
+    //     }
+    // }
+
+    if (jointFB_Flow != RTT::NoData) {
+        // if (!useRobotInertia) {
+        //     inertia_Port.write(inertia);
+        // }
+        h_Port.write(h);
+    }
+}
+
+bool InverseDynamicsTMP::startHook() {
+	return _models_loaded;
+}
+
+bool InverseDynamicsTMP::configureHook() {
+	return true;
+}
+
+std::vector<std::string> InverseDynamicsTMP::getKinematicChainNames() {
+	std::vector<std::string> names;
+	for (unsigned int i = 0; i < _xbotcore_model.get_chain_names().size();
+			++i) {
+		names.push_back(_xbotcore_model.get_chain_names()[i]);
+	}
+	return names;
+}
+
+bool InverseDynamicsTMP::selectKinematicChain(const std::string& chainName) {
+// Needs to be done in non-real-time only!
+
+	std::vector<std::string> enabled_joints_in_chain;
+	_xbotcore_model.get_enabled_joints_in_chain(chainName,
+			enabled_joints_in_chain);
+
+	RTT::log(RTT::Warning) << "Size of enabled joints: "
+			<< enabled_joints_in_chain.size() << RTT::endlog();
+
+// TODO do not hardcode this!
+	if (!p.initTreeAndChainFromURDFString(xml_string,  base_string,
+			tip_string, robot_tree, activeKDLChain)) {
+		log(Error) << "[ DLW " << this->getName()
+				<< "] URDF could not be parsed !" << endlog();
+
+		// TODO add proper error handling!
+        return false;
+	}
+	_models_loaded = true;
+
+	log(Info) << "[" << this->getName() << "] " << "robot_tree joints: "
+			<< robot_tree.getNrOfJoints() << ", robot_tree segments: "
+			<< robot_tree.getNrOfSegments() << endlog();
+
+	log(Info) << "[" << this->getName() << "] " << " activeKDLChain joints: "
+			<< activeKDLChain.getNrOfJoints() << ", activeKDLChain segments: "
+			<< activeKDLChain.getNrOfSegments() << RTT::endlog();
+
+    if(activeKDLChain.getNrOfJoints() != DOFsize){
+        log(Info) << "DOFsize " << DOFsize << " is different from urdf model" << RTT::endlog();
+        assert(false); //TODO
+        return false;
+    }
+
+	id_dyn_solver.reset(new KDL::ChainDynParam(activeKDLChain, gravity_vector));
+
+    jntPosConfigPlusJntVelConfig_q.resize(DOFsize);
+
+    gravity = Eigen::VectorXf(DOFsize,1);
+    coriolis = Eigen::VectorXf(DOFsize,1);
+
+    inertia = Eigen::MatrixXf(DOFsize,DOFsize);
+    inertia_Port.setDataSample(inertia);
+
+    h = Eigen::VectorXf(DOFsize);
+    h_Port.setDataSample(h);
+
+    M.resize(DOFsize);
+    C_.resize(DOFsize);
+    G_.resize(DOFsize);
+
+    jointFB = rstrt::robot::JointState(DOFsize);
+    jointFB.angles.fill(0);
+    return true;
+}
+
+bool InverseDynamicsTMP::loadURDFAndSRDF(const std::string &URDF_path,
+		const std::string &SRDF_path) {
+	if (!_models_loaded) {
+		std::string _urdf_path = URDF_path;
+		std::string _srdf_path = SRDF_path;
+
+		RTT::log(RTT::Info) << "URDF path: " << _urdf_path << RTT::endlog();
+		RTT::log(RTT::Info) << "SRDF path: " << _srdf_path << RTT::endlog();
+        assert(exists_test(_urdf_path) == true);
+        assert(exists_test(_srdf_path) == true);
+		_models_loaded = _xbotcore_model.init(_urdf_path, _srdf_path);
+
+		for (unsigned int i = 0; i < _xbotcore_model.get_chain_names().size();
+				++i) {
+//			RTT::log(RTT::Info) << "here: chain #" << i << " "
+//					<< _xbotcore_model.get_chain_names()[i] << RTT::endlog();
+			std::vector<std::string> enabled_joints_in_chain_i;
+			_xbotcore_model.get_enabled_joints_in_chain(
+					_xbotcore_model.get_chain_names()[i],
+					enabled_joints_in_chain_i);
+			for (unsigned int j = 0; j < enabled_joints_in_chain_i.size(); ++j)
+				RTT::log(RTT::Info) << "  " << enabled_joints_in_chain_i[j]
+						<< RTT::endlog();
+		}
+
+		xml_string = "";
+		std::fstream xml_file(URDF_path.c_str(), std::fstream::in);
+		if (xml_file.is_open()) {
+			while (xml_file.good()) {
+				std::string line;
+				std::getline(xml_file, line);
+				xml_string += (line + "\n");
+			}
+			xml_file.close();
+		}
+
+//		log(Warning) << xml_string << endlog();
+//		kdl_parser::treeFromUrdfModel(*(_xbotcore_model.get_urdf_model()), robot_tree);
+
+		activeKinematicChain = _xbotcore_model.get_chain_names()[0];
+		RTT::log(RTT::Warning) << "Set default chain: " << activeKinematicChain
+				<< RTT::endlog();
+
+		// setup kdl stuff
+//		robot_tree = _xbotcore_model.get_robot_tree();
+
+		selectKinematicChain(activeKinematicChain);
+	} else
+		RTT::log(RTT::Info) << "URDF and SRDF have been already loaded!"
+				<< RTT::endlog();
+
+	return _models_loaded;
+}
+
+void InverseDynamicsTMP::calculateDynamics(
+		const rstrt::robot::JointState& jointState) {
+
+//	jntPosConfigPlusJntVelConfig_q.q.data = jointState.angles.cast<double>();
+//    jntPosConfigPlusJntVelConfig_q.qdot.data = jointState.velocities.cast<double>();
+    this->castEigenVectorFtoD(jointState.angles, jntPosConfigPlusJntVelConfig_q.q.data);
+    this->castEigenVectorFtoD(jointState.velocities, jntPosConfigPlusJntVelConfig_q.qdot.data);
+
+	/* ### execute solver for Jacobian based on velocities */
+	// if (!useRobotInertia) {
+	// 	id_dyn_solver->JntToMass(jntPosConfigPlusJntVelConfig_q.q, M);
+	// }
+	id_dyn_solver->JntToGravity(jntPosConfigPlusJntVelConfig_q.q, G_);
+	id_dyn_solver->JntToCoriolis(jntPosConfigPlusJntVelConfig_q.q,
+			jntPosConfigPlusJntVelConfig_q.qdot, C_);
+
+
+    // this->castEigenMatrixDtoF(M.data, inertia);
+    this->castEigenVectorDtoF(G_.data, gravity);
+    this->castEigenVectorDtoF(C_.data, coriolis);
+
+    h = coriolis + gravity;
+}
+
+void InverseDynamicsTMP::computeGravity(rstrt::robot::JointState const & jointState, Eigen::VectorXf & gravity) {
+    this->castEigenVectorFtoD(jointState.angles, jntPosConfigPlusJntVelConfig_q.q.data);
+    this->castEigenVectorFtoD(jointState.velocities, jntPosConfigPlusJntVelConfig_q.qdot.data);
+
+    id_dyn_solver->JntToGravity(jntPosConfigPlusJntVelConfig_q.q, G_);
+
+    this->castEigenVectorDtoF(G_.data, gravity);
+}
+
+void InverseDynamicsTMP::castEigenVectorDtoF(Eigen::VectorXd const & d, Eigen::VectorXf & f) {
+    f = d.cast <float> ();
+}
+
+void InverseDynamicsTMP::castEigenVectorFtoD(Eigen::VectorXf const & f, Eigen::VectorXd & d) {
+    d = f.cast <double> ();
+}
+
+void InverseDynamicsTMP::castEigenMatrixDtoF(Eigen::MatrixXd const & d, Eigen::MatrixXf & f) {
+    f = d.cast <float> ();
+}
+
+void InverseDynamicsTMP::castEigenMatrixFtoD(Eigen::MatrixXf const & f, Eigen::MatrixXd & d) {
+    d = f.cast <double> ();
+}
+
+bool InverseDynamicsTMP::exists_test(const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void InverseDynamicsTMP::setBaseAndTip(std::string base,std::string tip){
+	this->base_string = base;
+	this->tip_string = tip;
+}
+void InverseDynamicsTMP::setGravityVector(double x, double y, double z){
+	gravity_vector = KDL::Vector(x,y,z);
+}
+
+ORO_LIST_COMPONENT_TYPE(cosima::InverseDynamicsTMP)
+
---
+++ src/inverse-dynamics-tmp.hpp
@@ -0,0 +1,150 @@
+#ifndef INVERSE_DYNAMICS_HPP
+#define INVERSE_DYNAMICS_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <rtt/Component.hpp>
+#include <rtt/os/Semaphore.hpp>
+
+#include <Eigen/Dense>
+
+#include <vector>
+
+#include <Eigen/Core>
+#include <time.h>
+#include <rtt/os/TimeService.hpp>
+#include <sstream>
+#include <rtt/Logger.hpp>
+
+#include <thread>
+#include <memory>
+
+#include <boost/shared_ptr.hpp>
+
+#include "KDLParser.hpp"
+
+#include <srdfdom_advr/model.h>
+#include <urdf/model.h>
+#include <XBotCoreModel.h>
+
+// KDL includes
+#include <kdl/tree.hpp>
+#include <kdl/chain.hpp>
+#include <kdl/jntarrayvel.hpp>
+#include <kdl/frames.hpp>
+#include <kdl/frames_io.hpp>
+#include <kdl/framevel.hpp>
+#include <kdl/framevel_io.hpp>
+#include <kdl/jacobian.hpp>
+
+#include <kdl/chainidsolver_recursive_newton_euler.hpp>
+#include <kdl/chaindynparam.hpp>
+#include <kdl/chainfksolvervel_recursive.hpp>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainjnttojacdotsolver.hpp>
+#include <kdl/chainfksolver.hpp>
+
+// RST-RT
+#include <rst-rt/robot/JointState.hpp>
+#include <rst-rt/kinematics/JointAngles.hpp>
+#include <rst-rt/kinematics/JointVelocities.hpp>
+#include <rst-rt/dynamics/JointTorques.hpp>
+
+namespace cosima {
+
+class InverseDynamicsTMP: public RTT::TaskContext {
+public:
+	InverseDynamicsTMP(std::string const& name);
+	bool configureHook();
+	void updateHook();
+	bool startHook();
+	void WorldUpdateBegin();
+	void WorldUpdateEnd();
+	void setBaseAndTip(std::string base, std::string tip);
+	void setGravityVector(double x, double y, double z);
+	virtual ~InverseDynamicsTMP() {
+	}
+
+protected:
+
+	void useRobotInertia_func(bool useRobotInertia);
+
+	/**
+	 * OutputPorts publish data.
+	 */
+    RTT::OutputPort<Eigen::MatrixXf> inertia_Port;
+	RTT::OutputPort<Eigen::VectorXf> h_Port;
+
+	/**
+	 * InputPorts read data.
+	 */
+	RTT::InputPort<rstrt::robot::JointState> jointFB_Port;
+	RTT::FlowStatus jointFB_Flow;
+	rstrt::robot::JointState jointFB;
+
+	// inertia from robot
+    RTT::InputPort<Eigen::MatrixXf> robotInertia_Port;
+	RTT::FlowStatus robotInertia_Flow;
+	bool useRobotInertia;
+
+	bool loadURDFAndSRDF(const std::string& URDF_path,
+			const std::string& SRDF_path);
+
+	std::vector<std::string> getKinematicChainNames();
+
+    bool selectKinematicChain(const std::string& chainName);
+
+    void calculateDynamics(const rstrt::robot::JointState& jointState);
+    void computeGravity(rstrt::robot::JointState const & jointState, Eigen::VectorXf & gravity);
+
+    // needs to be executed as very first operation
+	void setDOFsize(unsigned int DOFsize);
+
+	// needs to be executed as second operation
+	void setTranslationOnly(const bool translationOnly);
+
+    void castEigenVectorDtoF(Eigen::VectorXd const & d, Eigen::VectorXf & f);
+    void castEigenVectorFtoD(Eigen::VectorXf const & f, Eigen::VectorXd & d);
+    void castEigenMatrixDtoF(Eigen::MatrixXd const & d, Eigen::MatrixXf & f);
+    void castEigenMatrixFtoD(Eigen::MatrixXf const & f, Eigen::MatrixXd & d);
+    bool exists_test(const std::string& name);
+
+	XBot::XBotCoreModel _xbotcore_model;
+
+	std::string activeKinematicChain;
+
+    Eigen::MatrixXf inertia;
+    Eigen::VectorXf gravity;
+    Eigen::VectorXf coriolis;
+
+	// KDL stuff
+	KDL::Tree robot_tree;
+
+	KDL::JntArrayVel jntPosConfigPlusJntVelConfig_q;
+
+	KDL::JntSpaceInertiaMatrix M;
+	KDL::JntArray C_;
+	KDL::JntArray G_;
+    Eigen::VectorXf h;
+
+
+	KDL::Vector gravity_vector;
+
+	KDL::Chain activeKDLChain;
+
+	boost::shared_ptr<KDL::ChainDynParam> id_dyn_solver;
+
+	// Helper tools for KDL
+	KDLParser p;
+
+private:
+	bool _models_loaded;
+	std::string xml_string,base_string,tip_string;
+    unsigned int DOFsize;
+    bool receiveTranslationOnly;
+    unsigned int TaskSpaceDimension;
+};
+
+}
+#endif
